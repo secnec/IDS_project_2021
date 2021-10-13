66a6385a37c4258583c290abf52975f75023a953
Katia Chardon <katiachardon@outlook.fr>
2021-10-13 19:01:45 +0300

    Update on the website

--- katia/website/test_web.py
+++ katia/website/test_web.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def presentation():
+    return render_template("index.html")
+
+@app.route("/project")
+def project():
+    return render_template("index.html")
+
 if __name__ == "__main__":
    app.run(debug=True, host='0.0.0.0', port='5000')
